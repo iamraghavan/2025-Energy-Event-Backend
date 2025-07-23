ebc092e8e1e340a4ddb74679783e6463d78a0493
iamraghavan <developer.raghavan@gmail.com>
2025-07-23 22:03:50 +0530

    feat: Update player role schema to include detailed role options for cricket players

--- models/Player.js
+++ models/Player.js
@@ -3,7 +3,33 @@ const mongoose = require('mongoose');
 const playerSchema = new mongoose.Schema({
   name: { type: String, required: true },
   jerseyNumber: { type: Number, required: true },
-  role: { type: String, enum: ['batsman', 'bowler', 'allrounder', 'wicketkeeper'], required: true },
+  role: {
+    type: String,
+    required: true,
+    enum: [
+      // Batsmen
+      'Right-hand Batsman',
+      'Left-hand Batsman',
+      // Wicket-keepers
+      'Wicket-keeper',
+      // Bowlers
+      'Right-arm Fast Bowler',
+      'Right-arm Medium-fast Bowler',
+      'Right-arm Fast-medium Bowler',
+      'Left-arm Fast Bowler',
+      'Left-arm Medium-fast Bowler',
+      'Left-arm Fast-medium Bowler',
+      'Right-arm Off-spin Bowler (Off-break)',
+      'Right-arm Leg-spin Bowler (Leg-break/Googly)',
+      'Left-arm Orthodox Spinner',
+      'Left-arm Chinaman Spinner (Left-arm unorthodox spin)',
+      // All-rounders
+      'Batting All-rounder (Right-hand bat)',
+      'Batting All-rounder (Left-hand bat)',
+      'Bowling All-rounder (Fast/Medium)',
+      'Bowling All-rounder (Spin)'
+    ]
+  },
   team: { type: mongoose.Schema.Types.ObjectId, ref: 'Team', required: true }
 }, { timestamps: true });
 
